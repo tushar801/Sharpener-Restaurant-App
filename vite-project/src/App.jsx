4f82318e3451a764e4417090d419b8c37ef25296
tushar801 <jaintushar801@gmail.com>
2024-05-04 09:43:09 -0800

    Building the restaurant summary

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react"
 
 import Header from "./components/Layout/Header"
+import Meals from "./components/Meals/Meals"
 
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
   </Fragment>
   )
 }
